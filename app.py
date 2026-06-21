c9da68427658083027d4421475d5a3a15521d580
rmunab <rmunab25@gmail.com>
2026-06-20 22:37:37 -0400

    Preparar app para deploy en Render (gunicorn + Procfile)

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hola():
+def home():
     return "Hola Fuenti"
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
